4b403a090f9efca52035631a2b7c708e4209030b
suzen998 <hendzensinaga@gmail.com>
2020-07-21 20:48:20 +0700

    jajal2

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -26,7 +26,23 @@
             </div>
         </div>
         <div class="col-md-6">
-            grafik kegiatan Basket
+            <div class="card card-body border-info">
+                <h6>Grafik Kegiatan Siswa</h6>
+                <div class="row">
+                    <div class="col-md-8">
+                    <canvas id="dashSiswa" width="100" height="100"></canvas>
+                    </div>
+                    <div class="col-md-4">
+                        <h5><b>Tahun Ajaran</b></h5>
+                        5 Tahun Terakhir
+                        <div class="mb-3"></div>
+                        <h5><b>Kriteria</b></h5>
+                        Baik : <br>
+                        Kurang Baik : 
+                    </div>
+                </div>
+                <a href="<?=base_url('admin/')?>" class="btn btn-sm btn-info float-right mt-2">lihat detail</a>
+            </div>
         </div>
     </div>
 </div>
@@ -34,6 +50,22 @@
 <script>
 var dash = document.getElementById('dashGuru').getContext('2d');
 var dashGuru = new Chart(dash, {
+	type: 'pie',
+	data: {
+		labels: ['Baik', 'Kurang Baik'],
+		datasets: [{
+			label: '# of Votes',
+			data: [12, 19],
+			backgroundColor: [
+				'rgba(255, 99, 132, 0.2)',
+				'rgba(54, 162, 235, 0.2)'
+			],
+			borderWidth: 2
+		}]
+	}
+});
+var dash1 = document.getElementById('dashSiswa').getContext('2d');
+var dashSiswa = new Chart(dash1, {
 	type: 'pie',
 	data: {
 		labels: ['Baik', 'Kurang Baik'],
