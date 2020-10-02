6fadb494eaa6dfa0c1486b5cc9f8469ddb1c1be9
suzen998 <hendzensinaga@gmail.com>
2020-10-02 21:44:02 +0700

    jam hadir guru

--- application/models/presensi_model.php
+++ application/models/presensi_model.php
@@ -71,5 +71,20 @@ class Presensi_model extends CI_Model
         $ret = $query->row();
         return $ret->nama_guru;
     }
+    public function getJamhadirGuru($id)
+    {
+        return $this->db->query('SELECT id_guru,tahun_akademik,semester,tanggal, sec_to_time((SUM(jam_masuk)/COUNT(jam_masuk))-43200) AS rata_rata_jam_hadir_guru 
+        FROM presensi_harian WHERE id_guru='.$id.' GROUP BY id_guru,tahun_akademik DESC ,semester DESC LIMIT 4')->result_array();
+    }
+    public function getDefaultJamHadir($id)
+    {
+        // $query =$this->db->get_where('v_rata_jam_hadir', ['id_guru'=>$id]);
+        // $ret = $query->row();
+        //return $this->db->get_where('v_rata_jam_hadir',['id_guru'=> $id])->result_array();
+        //return $ret->rata_rata_jam_hadir_guru;
+        return $this->db->query('SELECT id_guru,tahun_akademik,semester,tanggal, sec_to_time((SUM(jam_masuk)/COUNT(jam_masuk))-43200) AS rata_rata_jam_hadir_guru 
+        FROM presensi_harian WHERE id_guru='.$id.' GROUP BY id_guru,tahun_akademik DESC ,semester DESC LIMIT 4')->result_array();
+    }
+    
 
 }
