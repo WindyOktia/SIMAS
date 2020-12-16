460486f7bdceb2b16445830b972d142bb57bb6a2
suzen998 <hendzensinaga@gmail.com>
2020-12-16 16:02:19 +0700

    nembak ip

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ date_default_timezone_set('Asia/Bangkok');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/SIMAS/';
+$config['base_url'] = 'http://192.168.1.9/SIMAS/';
 
 /*
 |--------------------------------------------------------------------------
